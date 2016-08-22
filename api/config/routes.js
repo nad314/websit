26398aae9d36342a570a49ce9f84977262c31d93
Borna Mihaljevic <borna.mihaljevic@gmail.com>
2016-08-22 21:14:46 +0200

    further lint fixes

--- api/config/routes.js
+++ api/config/routes.js
@@ -1,12 +1,13 @@
 /*jslint node: true*/
 /*jslint nomen: true*/
+/*jslint es5: true*/
 'use strict';
 
 var path = require("path");
 
 module.exports = function (express, app, controller) {
     app.use('/page', controller.page);
-    app.use('/css', express['static'](path.join(__dirname, "../../public/css")));
+    app.use('/css', express.static(path.join(__dirname, "../../public/css")));
     app.all('*', function (req, res) {
         return res.status(404).send('Page not found');
     });
