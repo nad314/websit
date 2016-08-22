e35d313a24616b23d67ef2872855306279c5909a
Borna Mihaljevic <borna.mihaljevic@gmail.com>
2016-08-22 02:19:30 +0200

    updated quotation marks

--- api/controller/page.js
+++ api/controller/page.js
@@ -2,12 +2,12 @@
 /*jslint nomen: true*/
 'use strict';
 
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-router.get("/", function (req, res) {
-    return res.sendFile(path.join(__dirname, "../../public/page/", "index.html"));
+router.get('/', function (req, res) {
+    return res.sendFile(path.join(__dirname, '../../public/page/', 'index.html'));
 });
 
 module.exports = router;
